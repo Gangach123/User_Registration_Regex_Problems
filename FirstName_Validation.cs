50bbc39d983e575536ad0e432068277e503e5dbe
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-06 12:12:49 +0530

    FirstNmae_Validation

---
+++ FirstName_Validation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex_Problems
+{
+    internal class FirstName_Validation
+    {
+        public static string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+
+
+        public void ValidateFirstName(string data)
+        {
+
+            string result = Regex.IsMatch(data, firstName) ? $"{data}: valid" : $"{data}: invalid";
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
